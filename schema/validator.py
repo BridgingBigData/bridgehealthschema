5d97c3bd94a5386090772bb366066195315240a2
Robin Gandhi <rgandhi@unomaha.edu>
2017-03-22 11:55:23 -0500

    restructure spall schema

--- schema/validator.py
+++ schema/validator.py
@@ -1,7 +1,7 @@
 import json
 from jsonschema import validate
 
-
+# Validate Cracks
 with open('crack.json') as data_file:
     data = json.load(data_file)
 
@@ -10,11 +10,20 @@ with open('crack.schema.json') as schema_file:
 
 validate(data,schema)
 
-
+# Validate Corrosion
 with open('corrosion.json') as data_file:
     data = json.load(data_file)
 
 with open('corrosion.schema.json') as schema_file:
+	schema = json.load(schema_file)
+
+validate(data,schema)
+
+# Validate Spalls
+with open('spall.json') as data_file:
+    data = json.load(data_file)
+
+with open('spall.schema.json') as schema_file:
 	schema = json.load(schema_file)
 
 validate(data,schema)
